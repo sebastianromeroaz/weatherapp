26c48996efdaf270dd21e2fdc3d76e025a01418c
sebastianaromeroaz <69485050+sebastianaromeroaz@users.noreply.github.com>
2021-12-16 14:10:16 -0700

    commit

--- src/components/weather.js
+++ src/components/weather.js
@@ -6,6 +6,10 @@ const CardExampleCard = ({weatherData}) => (
   <Card>
     <Card.Content>
         <Card.Header className="header">{weatherData.name}</Card.Header>
+        <p>Temperature: {weatherData.main.temp}</p>
+        <p>Sunrise : {weatherData.sys.sunrise}</p>
+        <p>Sunset: {weatherData.sys.sunset}</p>
+        <p>Description: {weatherData.weather[0].description}</p>
     </Card.Content>
   </Card>
 )
